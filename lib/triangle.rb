663cefa31810d42db8b1c6ce63605a3bce091b3b
nicmackenzie <nicmackenzie@live.com>
2023-07-12 11:32:30 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
 class Triangle
-  # write code here
+    def initialize(val1,val2,val3)
+      @val1 = val1
+      @val2 = val2
+      @val3 = val3
+    end
+
+    def kind
+      if (@val1 + @val2) <= @val3 || (@val2 + @val3) <= @val1 || (@val1 + @val3) <= @val2
+        raise TriangleError
+      elsif @val1 == 0 || @val2 == 0 || @val3 == 0
+        raise TriangleError
+      elsif @val1 < 0 || @val2 < 0 || @val3 < 0
+        raise TriangleError
+      elsif @val1 == @val2 && @val1 == @val3
+        :equilateral 
+      elsif @val1 == @val2 || @val1 == @val3 || @val2 == @val3
+        :isosceles
+      elsif @val1 != @val2 || @val1 != @val3 || @val2 != @val3
+        :scalene
+      end
+      # if @val1 == 0 || @val2 == 0 || @val3 == 0
+      #   raise TriangleError
+      # elsif @val1 < 0 || @val2 < 0 || @val3 < 0
+      #   raise TriangleError
+      # elsif @val1 == @val2 && @val1 == @val3
+      #   :equilateral 
+      # elsif @val1 == @val2 || @val1 == @val3 || @val2 == @val3
+      #   :isosceles
+      # elsif @val1 != @val2 || @val1 != @val3 || @val2 != @val3
+      #   :scalene
+      # end
+      
+      # if (@val1 + @val2) < @val3 || (@val2 + @val3) < @val1 || (@val1 + @val3) < @val2
+      #   raise TriangleError
+      # end
+    end
+  
+    class TriangleError < StandardError
+      def message
+        "Invalid traingle"
+      end
+    end
 end
